c4a05fceda22da516d809445594c682cc341fed4
Vivian Davila <viviandavilacodes@gmail.com>
2025-08-12 13:56:18 -0700

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/example/reward_chain/RewardChainApplication.java
@@ -0,0 +1,13 @@
+package com.example.reward_chain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardChainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reward_chain/RewardChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reward_chain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
