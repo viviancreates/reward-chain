666b33e6441e47fbb62b372d21fc1ee7e99e441c
Vivian Davila <viviandavilacodes@gmail.com>
2025-08-21 22:21:12 -0500

    Add config file

---
+++ src/main/java/com/example/reward_chain/controller/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.reward_chain.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+
+    }
+}
